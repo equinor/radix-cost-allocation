9034480590274919918903c5c4a6d8f7523bbf75
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2022-02-24 11:16:21 +0100

    185798 update go modules (#70)
    * update go modules
    
    * disable collection of old cost data
    
    Co-authored-by: Nils Gustav Stråbø <nst@equinor.com>

--- main.go
+++ main.go
@@ -27,7 +27,10 @@ func main() {
 	log.SetLevel(logLevel)
 
 	stopCh := make(chan struct{})
-	go run.InitAndStartOldDataCollector(appConfig.PrometheusAPI, appConfig.CronSchedule, appConfig.SQL, stopCh)
+
+	// Disable collection of the old data used for cost allocation
+	// go run.InitAndStartOldDataCollector(appConfig.PrometheusAPI, appConfig.CronSchedule, appConfig.SQL, stopCh)
+
 	go func() {
 		if err := run.InitAndStartCollector(appConfig.SQL, appConfig.Schedule, appConfig.AppNameExcludeList, stopCh); err != nil {
 			log.Fatal(err)
