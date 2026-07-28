310319394381a1d77407daa29387b790d24e19d7
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-07-28 12:32:03 +0200

    fix(deps): bump go version and dependencies

--- pkg/listers/testutils.go
+++ pkg/listers/testutils.go
@@ -2,10 +2,16 @@ package listers
 
 import "k8s.io/client-go/tools/cache"
 
-func setupFakeStoreForTest(listObjects ...interface{}) cache.Store {
-	return &cache.FakeCustomStore{
-		ListFunc: func() []interface{} {
-			return listObjects
-		},
-	}
+func setupFakeStoreForTest(listObjects ...any) cache.Store {
+	s := &fakeStore{}
+	s.ListFunc = func() []any { return listObjects }
+	return s
 }
+
+type fakeStore struct {
+	cache.FakeCustomStore
+}
+
+func (f *fakeStore) Bookmark(rv string) {}
+
+func (f *fakeStore) LastStoreSyncResourceVersion() string { return "" }
